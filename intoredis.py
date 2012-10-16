cf3d8be4c5eaa32d2a5cd214987b66221128a029
liuzhida <dannie0624@gmail.com>
2012-10-16 19:53:23 +0800

    into

--- intoredis.py
+++ intoredis.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 import redis
+import sys
 import helpers
 with open ("kfc.list","r") as data:
     webdata = data.read()
 
 c = redis.Redis(host='127.0.0.1', port=6379, db=0)
-print webdata
+#print webdata
 li = eval(webdata)
 #print li
 #li = webdata
 for json in li:
-    json = helpers.json_decode
+    #json = helpers.json_decode
     #json = eval(json)
     #c.lpush("dinner:kfc",json)
-    #print json['category']
+    word = json['category']
+    print word.decode('unicode')
     #for dish in json['dishes']:
     #    print dish['name']
     #    print dish['price']
-
