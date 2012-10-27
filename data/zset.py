5323cb5b86308321bc70aa96c10c496e7d9308fb
liuzhida <dannie0624@gmail.com>
2012-10-28 00:12:01 +0800

    sqlite

--- data/zset.py
+++ data/zset.py
@@ -17,24 +17,26 @@ import redis
 import helpers
 import time
 
+import tornado.database
+
 def main():
     c = redis.Redis(host='127.0.0.1', port=6379, db=1)
     json='{"id": "zhida@wandoujia.com","order": [{"name":"麦乐鸡套餐","from":"mac","number": "1", "price": "1650"},{"name":"可乐","from":"kfc","number": "1","price": "800"}]}'
 
     json = helpers.json_decode(json)
     id = json['id']
-    #c.zadd("dinner:user:pop",id,1)
     #c.zrem("dinner:user:pop",id)
     #print c.zrange("dinner:user:pop",0,-1,withscores=True)
     '''
     统计活跃用户
     '''
+    #c.zadd("dinner:user:pop",id,1)
     user_list = c.zrange("dinner:user:pop",0,-1)
     #print user_list
-    #if id in user_list:
-    #    c.zincrby("dinner:user:pop",id,1)
-    #else:
-    #    c.zadd("dinner:user:pop",id,1)
+    if id in user_list:
+        c.zincrby("dinner:user:pop",id,1)
+    else:
+        c.zadd("dinner:user:pop",id,1)
 
     str_time = time.strftime("%Y%m%d", time.localtime())
     #print "dinner:%s:%s"%(str_time,json['id'])
@@ -43,11 +45,12 @@ def main():
         print i
         rname = i['from']
         name = i['name']
+        print rname
 
         '''
         统计流行的餐厅
         '''
-        #c.zadd("dinner:from:pop",rname,1)
+        c.zadd("dinner:from:pop",rname,1)
         from_list = c.zrange("dinner:from:pop",0,-1)
         print "from_list:"
         print from_list
@@ -62,6 +65,8 @@ def main():
         '''    
         c.zadd("dinner:dish:pop",rname,1)
         dish_list = c.zrange("dinner:dish:pop",0,-1)
+        print "dish_list"
+        print dish_list
         #if name.encode('utf8') in dish_list:
         #    c.zincrby("dinner:dish:pop",name,1)
         #    print "ok"
@@ -79,20 +84,40 @@ def main():
         '''
         #c.lpush("dinner:%s:%s:%s"%(str_time,rname,name))
         
-
-def order():
+def add():
     c = redis.Redis(host='127.0.0.1', port=6379, db=1)
-    str_time = time.strftime("%Y%m%d", time.localtime())
-    #str_time = "20121022"
-
-    all_list = []
     all = c.lrange("dinner:list:all",0,-1)
     for _all in all:
         _all = helpers.json_decode(_all)
-        eval("%s={'from': '%s'}"%(_all['cname'],_all['name']))
-        eval("%s['order']=[]"%(_all['cname']))
-        
+        print _all['cname']
+        c.set("dinner:name:%s"%_all['cname'],_all['name']) 
 
+def order():
+    c = redis.Redis(host='127.0.0.1', port=6379, db=1)
+    #str_time = "20121022"
+
+    #all_list = []
+    #all = c.lrange("dinner:list:all",0,-1)
+    #for _all in all:
+    #    _all = helpers.json_decode(_all)
+    #    print _all['cname']
+    #    eval("%s = 'from': '%s'}"%(_all['cname'],_all['name']))
+    #    eval("%s['order']=[]"%(_all['cname']))
+    #    
+    zheng   = {} 
+    yonghe  = {}
+    dou     = {}
+    hehegu  = {}
+    meiming = {}
+    zhen    = {}
+    kfc     = {}
+    jiahe   = {}
+    jiye    = {}
+    kang    = {}
+    mac     = {}
+    pizza   = {}
+
+    str_time = time.strftime("%Y%m%d", time.localtime())
     li = c.keys("dinner:%s:*"%str_time)
     for i in li:
         id = i.split(':')[2]
@@ -100,13 +125,20 @@ def order():
         _li = c.lrange(i,0,-1)
         for _i in _li:
             _i = helpers.json_decode(_i)
-            froms   = _i['from']
+            fname   = _i['from']
             name    = _i['name']
             number  = _i['number']
             price   = _i['price']
-            eval("orders = %s['order']"%_i['from'])
-            order = {}
-            order['dish']=""
+
+            orders = []
+            if fname == "kfc":
+                froms = c.get("dinner:name:kfc")
+                print froms
+                kfc['from'] = froms
+                order = {}
+                order['dish'] = name
+                kfc['order']
+    
 '''
 [
     {
@@ -129,6 +161,7 @@ def order():
 if __name__ == '__main__':
     #main()
     order()
+    #add()
 '''
 {
     "id": "zhida@wandoujia.com",
