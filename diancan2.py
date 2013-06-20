99cb9e6afe1fd270378b56c5cd885f0c0c4bcc1f
liuzhida <dannie0624@gmail.com>
2013-06-20 16:53:51 +0800

    diancan2 change

--- diancan2.py
+++ diancan2.py
@@ -102,7 +102,8 @@ class DelOrderHandler(BaseHandler):
         if not self.current_user:
             raise tornado.web.HTTPError(403)
             return
-        c = redis.Redis(host='127.0.0.1', port=6379, db=8)
+        c = redis.Redis(host='127.0.0.1', port=6379, db=1)
+        _c = redis.Redis(host='127.0.0.1', port=6379, db=8)
         cx = sqlite3.connect("/home/work/diancan2/data/dinner.db")
         cu = cx.cursor()
         self.user = tornado.escape.json_decode(self.current_user)
@@ -110,10 +111,38 @@ class DelOrderHandler(BaseHandler):
         str_time = time.strftime("%Y%m%d", time.localtime())
         bid = base64.encodestring(id.encode("utf-8")).strip()
         day = int(str_time)
+
+        
+        allorder = "dinner:%s:%s" % (str_time, id)
+        _order = c.lrange(allorder, 0, -1)
+        #orders = []
+        for o in _order:
+            o = json.loads(o)
+            rname = o['from']
+            name = o['name']
+            number = o['number']
+
+
+            food = _c.hgetall("dinner:rest:"+str_time+":"+rname.encode("utf-8"))
+            if name.encode("utf-8") in food:
+                item = eval(food[name.encode("utf-8")])
+                _number = float(item['Quantity'])
+                _item = item.copy()
+                _item['Quantity'] = _number - float(number)
+                if _item['Quantity']:
+                    c.hset("dinner:rest:%s:%s" % (str_time, rname), name, _item)
+                    _c.hset("dinner:rest:%s:%s" % (str_time, rname), name, _item)
+                else:
+                    c.delete("dinner:rest:%s:%s" % (str_time, rname), name)
+                    _c.delete("dinner:rest:%s:%s" % (str_time, rname), name)
+
         c.delete("dinner:%s:%s" % (str_time, id))
+        _c.delete("dinner:%s:%s" % (str_time, id))
         cu.execute('delete from orders where id = ? and day =?', (bid, day))
         cx.commit()
         return self.finish("successuflly delete %s's dinner" % id.split("@")[0])
+            #else:
+            #    self.write(json.dumps(food))
 
 
 class OrderHandler(BaseHandler):
@@ -144,7 +173,8 @@ class OrderHandler(BaseHandler):
         return self.finish(all)
 
     def post(self):
-        c = redis.Redis(host='127.0.0.1', port=6379, db=8)
+        c = redis.Redis(host='127.0.0.1', port=6379, db=1)
+        _c = redis.Redis(host='127.0.0.1', port=6379, db=8)
         cx = sqlite3.connect("/home/work/diancan2/data/dinner.db")
         cu = cx.cursor()
         data = self.get_argument('json')
@@ -155,9 +185,9 @@ class OrderHandler(BaseHandler):
             raise tornado.web.HTTPError(403)
             return
         dead = int(time.strftime("%H%M", time.localtime()))
-        #if dead >= 1559:
-        #   raise tornado.web.HTTPError(403)
-        #   return
+        if dead >= 1555:
+           raise tornado.web.HTTPError(403)
+           return
 
         str_time = time.strftime("%Y%m%d", time.localtime())
         for i in data['order']:
@@ -175,6 +205,7 @@ class OrderHandler(BaseHandler):
             '''
             li = json.dumps(i)
             c.lpush("dinner:%s:%s" % (str_time, data['id']), li)
+            _c.lpush("dinner:%s:%s" % (str_time, data['id']), li)
 
             '''
             到家接口
@@ -189,13 +220,15 @@ class OrderHandler(BaseHandler):
                     _item = item.copy()
                     _item['Quantity'] = _number + float(number)
                     c.hset("dinner:rest:%s:%s" % (str_time, rname), name, _item)
+                    _c.hset("dinner:rest:%s:%s" % (str_time, rname), name, _item)
                 else:
                     item = dict()
-                    item['Foodname'] = name
+                    item['FoodName'] = name
                     p = float(price) / 100
                     item['Price'] = str("%.2f" % p)
                     item['Quantity'] = float(number)
                     c.hset("dinner:rest:%s:%s" % (str_time, rname), name, item)
+                    _c.hset("dinner:rest:%s:%s" % (str_time, rname), name, item)
             else:
                 item = dict()
                 item['FoodName'] = name
@@ -203,6 +236,7 @@ class OrderHandler(BaseHandler):
                 item['Price'] = str("%.2f" % p)
                 item['Quantity'] = float(number)
                 c.hset("dinner:rest:%s:%s" % (str_time, rname), name, item)
+                _c.hset("dinner:rest:%s:%s" % (str_time, rname), name, item)
 
             cu.execute(
                 'insert into orders (id,froms,dish,number,price,day) values(?,?,?,?,?,?)',
@@ -302,6 +336,29 @@ class AllOrderHandler(tornado.web.RequestHandler):
         return self.render('alll.html', li=all_list, p=npeople,)
 
 
+class EachOrderHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        str_time = time.strftime("%Y%m%d", time.localtime())
+        _c = redis.Redis(host='127.0.0.1', port=6379, db=8)
+        c = redis.Redis(host='127.0.0.1', port=6379, db=1)
+        keys = _c.keys("dinner:%s:*"%str_time)
+        dinner = list()
+        for key in keys:
+            id = key.split(":")[-1]
+            _orders = _c.lrange(key,0,-1) 
+            order = list()
+            for o in _orders:
+                order.append(json.loads(o))
+            person = dict() 
+            person['order'] = order
+            person['name'] = c.get("dinner:cname:%s"%id)
+            dinner.append(person)
+
+        self.set_header("Content-Type", "text/html")
+        return self.render('order.html', all=dinner)
+
+
 class UserHandler(BaseHandler):
 
     @tornado.web.authenticated
@@ -309,7 +366,7 @@ class UserHandler(BaseHandler):
         if not self.current_user:
             raise tornado.web.HTTPError(403)
             return
-        c = redis.Redis(host='127.0.0.1', port=6379, db=1)
+        c = redis.Redis(host='127.0.0.1', port=6379, db=8)
         self.user = tornado.escape.json_decode(self.current_user)
         email = tornado.escape.xhtml_escape(self.user["email"])
         # email = tornado.escape.xhtml_escape(self.current_user["email"])
@@ -332,7 +389,7 @@ class UserHandler(BaseHandler):
         if not name:
             c.delete("dinner:cname:%s" % id)
         else:
-            c = redis.Redis(host='127.0.0.1', port=6379, db=1)
+            c = redis.Redis(host='127.0.0.1', port=6379, db=8)
             c.set("dinner:cname:%s" % id, name)
         cname = c.get("dinner:cname:%s" % id)
         user = {}
@@ -363,14 +420,15 @@ def main():
     application = tornado.web.Application([
         (r"/", IndexHandler),
         #(r"/",                  MainHandler),
-        (r"/api/login", GoogleAuthLoginHandler),
-        (r"/api/logout", LogoutHandler),
-        (r"/api/all", AllHandler),
-        (r"/api/order", OrderHandler),
-        (r"/api/delorder", DelOrderHandler),
-        (r"/api/allorder", AllOrderHandler),
-        (r"/api/user", UserHandler),
-        (r"/api/data/(.*)", DataHandler),
+        (r"/api/login",         GoogleAuthLoginHandler),
+        (r"/api/logout",        LogoutHandler),
+        (r"/api/all",           AllHandler),
+        (r"/api/order",         OrderHandler),
+        (r"/api/delorder",      DelOrderHandler),
+        (r"/api/allorder",      AllOrderHandler),
+        (r"/api/orders",        EachOrderHandler),
+        (r"/api/user",          UserHandler),
+        (r"/api/data/(.*)",     DataHandler),
     ], **settings)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python diancan2.py --port=9001 1>2.log 2>&1 &
