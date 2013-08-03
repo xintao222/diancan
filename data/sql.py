2c42a6c80add0d23c5c2a491d22dbfea22e6210a
zhida <zhida@wandoujia.com>
2013-08-03 10:19:25 +0800

    delete info

--- data/sql.py
+++ data/sql.py
@@ -7,7 +7,7 @@ import json
 
 
 def orders():
-    cx = sqlite3.connect("/home/work/diancan/data/dinner2.db")
+    cx = sqlite3.connect("/home/work/diancan/data/dinner.db")
     cu = cx.cursor()
     cu.execute(
         'create table orders (uid integer primary key autoincrement, id varchar(128),froms varchar(128),dish varchar(128),number integer, price integer, day integer)')
